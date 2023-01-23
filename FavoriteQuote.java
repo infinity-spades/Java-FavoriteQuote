c9190da6f2379cb241f06cbcf87f9c695a3e5361
Srishti Pal <33604510+infinity-spades@users.noreply.github.com>
2023-01-22 23:24:19 -0800

    source code and byte code
    Hello World level program

---
+++ FavoriteQuote.java
@@ -0,0 +1,7 @@
+class FavoriteQuote {
+    // A Java program begins with a call to main()
+    public static void main(String[] args) {
+        String output = "If you want to shine like a sun, first burn like a sun.\r\n\t-Dr. A.P.J. Abdul Kalam";
+        System.out.println(output);
+    }
+}
